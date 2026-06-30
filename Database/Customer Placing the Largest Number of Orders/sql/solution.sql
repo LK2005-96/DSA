3cc00d3ae788428b7764b0908d18a51d192ac39d
LK2005-96 <lkdon4731f@gmail.com>
2026-06-30 15:22:07 +0530

    feat: Add Customer Placing the Largest Number of Orders

--- Database/Customer Placing the Largest Number of Orders/sql/solution.sql
+++ Database/Customer Placing the Largest Number of Orders/sql/solution.sql
@@ -1,2 +1,5 @@
 # Write your MySQL query statement below
-select customer_number from Orders order by order_number desc limit 1;
+select customer_number from Orders
+group by customer_number
+order by count(customer_number) desc
+limit 1;
