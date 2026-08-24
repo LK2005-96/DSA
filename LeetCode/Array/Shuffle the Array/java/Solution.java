8f77fa6df85062d6ce00a1d978b928abc0eb8b24
LK2005-96 <lkdon4731f@gmail.com>
2026-08-24 13:33:41 +0530

    feat: Add Shuffle the Array

--- LeetCode/Array/Shuffle the Array/java/Solution.java
+++ LeetCode/Array/Shuffle the Array/java/Solution.java
@@ -1,18 +1,16 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int arr[]=new int[nums.length];
-        int x1=0;
-        int y1=1;
-        int in1=0;
-        int in2=n;
+        int j=0;
         for(int i=0;i<nums.length;i=i+2)
         {
-           arr[x1]=nums[in1];
-           arr[y1]=nums[in2];
-           x1+=2;
-           y1+=2;
-           in1++;
-           in2++;
+           arr[i]=nums[j];
+           j++;
+        }
+        for(int i=1;i<nums.length;i=i+2)
+        {
+            arr[i]=nums[j];
+           j++;
         }
         return arr;
     }
