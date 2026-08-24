8ef36002068b8dbfac48fc0cb9583df81602a9e9
LK2005-96 <lkdon4731f@gmail.com>
2026-08-24 15:29:39 +0530

    feat: Add Stone Game VIII

--- LeetCode/General/Stone Game VIII/cpp/solution.cpp
+++ LeetCode/General/Stone Game VIII/cpp/solution.cpp
@@ -1,15 +1,14 @@
 1class Solution {
 2public:
 3    int stoneGameVIII(vector<int>& stones) {
-4        int sum = 0;
-5        for (int x : stones) {
-6            sum += x;
-7        }
-8        int r = sum;
-9        for (int i = stones.size() - 2; i; --i) {
-10            sum -= stones[i + 1];
-11            r = max(r, sum - r);
-12        }
-13        return r;
-14    }
-15};
+4        int n = (int)stones.size();
+5        for(int i = 1; i < n; ++i)
+6            stones[i] += stones[i - 1];
+7        vector<int> dp(n);
+8        dp[n - 1] = stones[n - 1];
+9        for(int i = n - 2; i > 0; --i) {
+10            dp[i] = max(dp[i + 1], stones[i] - dp[i + 1]);
+11        }
+12        return dp[1];
+13    }
+14};
