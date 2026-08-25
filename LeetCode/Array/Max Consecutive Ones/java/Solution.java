026d7d7c3f63285e0c2999d0f538a1cb7d080144
LK2005-96 <lkdon4731f@gmail.com>
2026-08-25 09:19:31 +0530

    feat(leetcode): Sync String to Integer (atoi) and 9 others

--- LeetCode/Array/Max Consecutive Ones/java/Solution.java
+++ LeetCode/Array/Max Consecutive Ones/java/Solution.java
@@ -1,16 +1,17 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-         int count=0;
-        int count2=0;
-        for (int i=0;i<nums.length;i++)
+        int max=0;
+        int count=0;
+        for(int i:nums)
         {
-            if(nums[i]==1)
-             count++;
+            if(i==1) count++;
             else 
-             {count=0;}
-            count2=Math.max(count2,count);
-
+            {
+                max=Math.max(count,max);
+                count=0;
+            }
         } 
-        return count2;
+        max=Math.max(count,max);
+        return max;
     }
 }
--- LeetCode/Array/Next Greater Element I/java/Solution.java
+++ LeetCode/Array/Next Greater Element I/java/Solution.java
@@ -1,24 +1,32 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        for(int j=0;j<nums1.length;j++)
-        {   int target=nums1[j];
-            int index = IntStream.range(0, nums2.length)
-                     .filter(i -> nums2[i] == target)
-                     .findFirst()
-                     .orElse(-1);
-            boolean k=false;
-            for(int l=index+1;l<nums2.length;l++)
-            {
-                if(nums2[l]>nums1[j])
-                {
-                    nums1[j]=nums2[l];
-                    k=true;
-                    break;
-                }
+        int maxVal = 0;
+        for (int num : nums2) {
+            if (num > maxVal) {
+                maxVal = num;
             }
-            if(!k)  nums1[j]=-1;
-            if(index==nums2.length-1) nums1[j]=-1;
+        }
+        int[] map = new int[maxVal + 1];
+        int [] stack=new int[nums2.length];
+        int top = -1;
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            int current = nums2[i];
+            while (top>=0 && stack[top] <= current) {
+                top--;
+            }
+            if (top==-1) {
+                map[current] = -1;
+            } else {
+                map[current] = stack[top];
+            }
+            stack[++top]=current;
+        }
+       // System.out.print(stack);
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map[nums1[i]];
+        }
 
-        } return nums1;
+        return ans;
     }
 }
