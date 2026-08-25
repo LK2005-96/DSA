eb3b37a9f98a29324a5ee36fd8107b5940ae7865
LK2005-96 <lkdon4731f@gmail.com>
2026-08-25 16:11:03 +0530

    feat: Add Sort Colors

--- LeetCode/Array/Sort Colors/java/Solution.java
+++ LeetCode/Array/Sort Colors/java/Solution.java
@@ -1,23 +1,20 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int zero = 0, one = 0, two = 0;
-        for (int n : nums) {
-            if (n == 0) zero++;
-            else if (n == 1) one++;
+        int n=nums.length;
+       int zero=0;
+       int one=0;
+       int two=0;
+       for(int i=0;i<n;i++)
+       {
+            if(nums[i]==0) zero++;
+            else if(nums[i]==1) one++;
             else two++;
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            if (zero > 0) {
-                nums[i] = 0;
-                zero--;
-            } else if (one > 0) {
-                nums[i] = 1;
-                one--;
-            } else {
-                nums[i] = 2;
-                two--;
-            }
-        }
+       }
+       for(int i=0;i<n;i++)
+       {
+           if(i<zero ) nums[i]=0;
+           else if(i<zero+one) nums[i]=1;
+           else nums[i]=2;
+       }
     }
 }
