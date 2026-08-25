d875966651342f794377b3fb39ddf101899cd0c0
LK2005-96 <lkdon4731f@gmail.com>
2026-08-25 15:37:46 +0530

    feat: Add Majority Element

--- LeetCode/Array/Majority Element/java/Solution.java
+++ LeetCode/Array/Majority Element/java/Solution.java
@@ -1,13 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-       
-         int count=0;
-         int candidate=0;
-         for(int num:nums)
-         {
-            if(count==0) candidate=num;
-            if(candidate==num) count++;
-            else count--;
-         }return candidate;
+        Map<Integer,Integer> map=new HashMap<>();
+        int ans=0;
+        int max=0;
+        for(int i=0;i<nums.length;i++)
+        {
+             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+             if(map.get(nums[i])>max)
+             {
+                max=map.get(nums[i]);
+                ans=nums[i];
+             }
+        }return ans;
     }
 }
