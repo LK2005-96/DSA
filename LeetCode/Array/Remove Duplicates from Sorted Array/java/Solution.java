8a19390dd0b9e88273f996d5888e7b354860e858
LK2005-96 <lkdon4731f@gmail.com>
2026-08-25 15:22:13 +0530

    feat: Add Remove Duplicates from Sorted Array

--- LeetCode/Array/Remove Duplicates from Sorted Array/java/Solution.java
+++ LeetCode/Array/Remove Duplicates from Sorted Array/java/Solution.java
@@ -1,17 +1,22 @@
-public class Solution {
+class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length == 0) {
-            return 0;
-        }
-
-        int k = 1; // Initialize the count of unique elements to 1
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[k - 1]) {
-                nums[k] = nums[i]; // Overwrite the next unique element
-                k++;
+        int arr[]=new int[nums.length];
+        int index=1;
+        int prev=0;
+        arr[0]=nums[0];
+        for(int i=1;i<nums.length;i++)
+        {
+            if(arr[prev]!=nums[i])
+            {  arr[index]=nums[i];
+                prev=index;
+               index++;
             }
         }
-        
-        return k;
+        for(int i=0;i<nums.length;i++)
+        {
+            nums[i]=arr[i];
+        }
+        System.out.print(Arrays.toString(arr));
+        return prev+1;
     }
 }
