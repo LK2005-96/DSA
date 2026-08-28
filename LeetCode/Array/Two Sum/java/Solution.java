2c5b1497bfff940cb81dc8287451105a55d7a4e3
Lalith Kishore S A <lkdon4731f@gmail.com>
2026-08-28 09:15:13 +0530

    feat(leetcode): Sync Lexicographically Smallest Permutation Greater Than Target and 9 others

--- LeetCode/Array/Two Sum/java/Solution.java
+++ LeetCode/Array/Two Sum/java/Solution.java
@@ -1,13 +1,17 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
-        }
-        return new int[]{}; // No solution found
-    }
-}
+    public int[] twoSum(int[] arr, int target) {
+        int ar[]=new int[2];
+        for(int i=0;i<arr.length;i++)
+		{
+		    int get=target-arr[i];
+		    for(int j=i+1;j<arr.length;j++)
+		    {
+		        if(arr[j]==get) 
+		        {
+		            ar[0]=i;
+                    ar[1]=j;
+                    break;
+		        }
+		    }
+         } return ar;
+}}
